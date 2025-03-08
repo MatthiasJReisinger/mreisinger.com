78d7ee33ed6b6bd1fa8c12f94ce61344c0803415
Matthias Reisinger <mail@mreisinger.com>
2025-03-08 18:13:48 +0100

    Add basic styling to headers in mdx files

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,13 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h2: ({ children }) => (
+      <h2 className="text-xl font-bold pt-10 pb-5">{children}</h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="text-l font-bold pt-5 pb-3">{children}</h3>
+    ),
+
     ...components,
   };
 }
