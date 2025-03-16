80ccf0e4670543340d464965e09bd1f216a861be
Matthias Reisinger <mail@mreisinger.com>
2025-03-16 14:24:27 +0100

    Remove unnecessary heading

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -33,9 +33,6 @@ export default async function Page() {
 
   return (
     <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
       {posts.map((post) => (
         <PostCard key={post.postId} post={post} />
       ))}
