893f96ca5fbed4d2b5f59854165909fb53173120
Matthias Reisinger <mail@mreisinger.com>
2025-03-22 18:01:31 +0100

    Add imprint page

---
+++ src/app/imprint/page.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div>
+      <h2 className="font-bold text-3xl mb-5">Imprint</h2>
+      <div>Matthias Reisinger</div>
+      <div>Kreuzgasse 50/27-28, 1180 Vienna, Austria</div>
+      <div>Tel: +43 699 17127796</div>
+      <div>E-Mail: mail@mreisinger.com</div>
+    </div>
+  );
+}
