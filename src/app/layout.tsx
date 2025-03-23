dda685d789ec7243cb6ea26eee8d6365204dd4bf
Matthias Reisinger <mail@mreisinger.com>
2025-03-23 16:38:47 +0100

    Add minimal effort responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col min-h-screen mx-auto w-full max-w-screen-lg">
+        <div className="flex flex-col min-h-screen mx-auto w-full max-w-screen-lg px-5 md:px-0">
           <NavBar />
           <main className="flex-1">{children}</main>
           <Footer />
