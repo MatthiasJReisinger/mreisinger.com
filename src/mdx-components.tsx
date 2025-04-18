6fedcaee0cec597de064bb0c080a17b4290a5d9a
Matthias Reisinger <mail@mreisinger.com>
2025-04-18 20:54:43 +0200

    Add paragraph padding in blog posts

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h2: ({ children }) => <h2 className="pt-10 pb-5">{children}</h2>,
     h3: ({ children }) => <h3 className="pt-5 pb-3">{children}</h3>,
+    p: ({ children }) => <p className="pb-3">{children}</p>,
     ...components,
   };
 }
